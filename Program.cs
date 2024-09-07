f7e5252a7d864a14d21f3e04217ff8da12240ab7
Nathalia Monteles <nathalia_martinsm@hotmail.com>
2024-09-07 17:11:34 -0300

    Fim desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
+Nokia meuNokia = new Nokia("123456789", "Nokia 3310", "IMEI123456789", 32);
+meuNokia.Ligar();
+meuNokia.InstalarAplicativo("Instagran");
+
+Iphone meuIphone = new Iphone("987654321", "Iphone 12", "IMEI987654321", 128);
+meuIphone.ReceberLigacao();
+meuIphone.InstalarAplicativo("WhatsApp");
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
